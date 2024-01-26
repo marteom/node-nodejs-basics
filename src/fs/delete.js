4ced4d4cd3d61669e795cc35d1cc2a9759118898
marteom <marteom@gmail.com>
2024-01-26 13:27:04 +0300

    feat: add solution for delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,32 @@
+import { rm, access } from 'node:fs/promises';
+
+const checkExist = async (path) => {
+    try {
+        const checkResult = await access(path);
+        if(!checkResult) {
+            return true;
+        }
+    }
+    catch {
+        return false;
+    }
+}
+
 const remove = async () => {
-    // Write your code here 
+    const sourceFile = new URL('files/fileToRemove.txt', import.meta.url);
+
+    const isSourceFileExist = await checkExist(sourceFile);
+
+    if(!isSourceFileExist) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await rm(sourceFile);
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
