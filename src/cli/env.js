1eacd6903e7028826deeb17ec99671fe1ccd64cd
marteom <marteom@gmail.com>
2024-01-26 14:57:08 +0300

    feat: add solution for env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envEntries = Object.entries(process.env);
+
+    let resultArr = [];
+
+    for (const [key, value] of envEntries) {
+        if (key.indexOf('RSS_') === 0) {
+            resultArr.push(`${key}=${value}`);
+        }
+    }
+
+    console.log(resultArr.join('; '));
 };
+
+//process.env.RSS_test = '111';
+//process.env.RSS_test2 = '222';
 
 parseEnv();
