34a68d1bf3bd1d994669bc4e7dcb946dc7ac9008
marteom <marteom@gmail.com>
2024-01-26 15:33:09 +0300

    feat: add solution for read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
-};
+    const filePath = new URL('files/fileToRead.txt', import.meta.url);
+    const readableStream = fs.createReadStream(filePath);
+    readableStream.setEncoding('utf8');
+    let data = '';
+
+    try {
+        for await (const chunk of readableStream) {
+            data += chunk;
+        }
+        process.stdout.write(data + '\n');
+    }
+    catch(err) {
+        process.stderr.write(err.message + '\n');
+    }
+}
 
 await read();
