60fcfdced50da0b0b3d124b15bd36299d593388a
marteom <marteom@gmail.com>
2024-01-26 15:43:18 +0300

    feat: add solution for decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import zlib from 'node:zlib';
+import fs from 'fs';
+import { pipeline } from 'node:stream';
+
 const decompress = async () => {
-    // Write your code here 
+
+    const inputZipPath = new URL('files/archive.gz', import.meta.url);
+    const OutputfilePath = new URL('files/fileToCompress.txt', import.meta.url);
+
+    const unzip = zlib.createUnzip();
+    const input = fs.createReadStream(inputZipPath);
+    const output = fs.createWriteStream(OutputfilePath);
+
+    pipeline(input, unzip, output, (error) => {
+        if (error) console.log(error);
+    });
+
 };
 
 await decompress();
