c03e0b3faae0b1788ce49778728af9ca4d5e0b89
marteom <marteom@gmail.com>
2024-01-26 13:02:45 +0300

    feat: add solution for create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { access, writeFile } from 'fs/promises';
+
+const checkExist = async (fullFileName) => {
+    try {
+        const checkResult = await access(fullFileName);
+        if(!checkResult) {
+            return true;
+        }
+    }
+    catch {
+        return false;
+    }
+}
+
 const create = async () => {
-    // Write your code here 
+    const fullFileName = new URL('./files/fresh.txt', import.meta.url);
+
+    const isFileExist = await checkExist(fullFileName);
+
+    if(isFileExist) {
+        throw new Error('FS operation failed');
+    }
+    else {
+        const content = 'I am fresh and young';
+        await writeFile(fullFileName, content);      
+    }
 };
 
 await create();
