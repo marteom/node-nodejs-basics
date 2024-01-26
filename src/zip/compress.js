bfc036d8659c63d5f603b3fba0d30c64df4e4594
marteom <marteom@gmail.com>
2024-01-26 15:41:41 +0300

    feat: add solution for compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'node:zlib';
+import fs from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+
+  const inputFilePath = new URL('files/fileToCompress.txt', import.meta.url);
+  const ZipfilePath = new URL('files/archive.gz', import.meta.url);
+
+    await pipeline(
+        fs.createReadStream(inputFilePath),
+        createGzip(),
+        fs.createWriteStream(ZipfilePath)
+      );
 };
 
 await compress();
