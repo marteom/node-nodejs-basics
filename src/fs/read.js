f818cbe72f5a1f864a4ea5ae4e8e817f41900df1
marteom <marteom@gmail.com>
2024-01-26 13:30:58 +0300

    feat: add solution for read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import { access, readFile } from 'node:fs/promises';
+
+const checkExist = async (fullFileName) => {
+    try {
+        const checkResult = await access(fullFileName);
+        if(!checkResult) {
+            return true;
+        }
+    }
+    catch {
+        return false;
+    }
+}
+
 const read = async () => {
-    // Write your code here 
+    const fullFileName = new URL('files/fileToRead.txt', import.meta.url);
+
+    const isFileExist = await checkExist(fullFileName);
+
+    if(!isFileExist) {
+        throw new Error('FS operation failed');
+    }
+    else {
+        const fileContent = await readFile(fullFileName, {encoding: "utf8"});
+        console.log(fileContent);      
+    }
 };
 
 await read();
