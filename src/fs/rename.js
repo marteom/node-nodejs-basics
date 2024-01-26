13b54180f681915525522529f15b2248f2fdcba8
marteom <marteom@gmail.com>
2024-01-26 13:19:16 +0300

    feat: add solution for rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import { rename as fs_rename, access } from 'node:fs/promises';
+
+const checkExist = async (path) => {
+    try {
+        const checkResult = await access(path);
+        if(!checkResult) {
+            return true;
+        }
+    }
+    catch {
+        return false;
+    }
+}
+
 const rename = async () => {
-    // Write your code here 
+    const sourceFile = new URL('files/wrongFilename.txt', import.meta.url);
+    const destFile = new URL('files/properFilename.md', import.meta.url);
+
+    const isSourceFileExist = await checkExist(sourceFile);
+    const isDestFileExist = await checkExist(destFile);
+
+    if(!isSourceFileExist || isDestFileExist) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await fs_rename(sourceFile, destFile);
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
