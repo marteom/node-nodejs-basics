84be6d24b1e3e1233d27335a1c3de8ca80602c8a
marteom <marteom@gmail.com>
2024-01-26 15:13:38 +0300

    feat: add solution for calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'node:crypto'
+import { readFile } from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try{
+        const filePath = new URL('./files/fileToCalculateHashFor.txt', import.meta.url);
+        const fileContent = await readFile(filePath, {encoding: "utf8"});
+        const generatedHash = createHash('sha256').update(fileContent).digest('hex');
+        console.log(generatedHash);
+    }
+    catch(ex) {
+        console.log(ex.message);
+    }
 };
 
 await calculateHash();
