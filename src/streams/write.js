92469aac6976671e6d91bac9fa480b4ac3d8a9ae
marteom <marteom@gmail.com>
2024-01-26 15:34:31 +0300

    feat: add solution for write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import readlinePromises from 'readline/promises';
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = new URL('files/fileToWrite.txt', import.meta.url);
+
+    const outputStream = fs.createWriteStream(filePath, {flags: 'a'});
+    
+    const rl = readlinePromises.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    try {
+        for await (const line of rl) {
+            outputStream.write(line + '\r\n');
+        }
+    }
+    catch(err) {
+        process.stderr.write(err.message);
+    }
+
 };
 
 await write();
