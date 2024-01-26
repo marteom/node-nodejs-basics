a682f04e10027cd18d0e5a1c41b7585af7bb8d34
marteom <marteom@gmail.com>
2024-01-26 14:59:43 +0300

    feat: add solution for args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    let resultArr = [];
+
+    const args = process.argv;
+    for(let i=0; i<args.length; i++) {
+        if (args[i].indexOf('--') === 0){
+            resultArr.push(`${args[i].substring(2)} is ${args[i+1]}`);
+        }
+    }
+    
+    console.log(resultArr.join(', '));
 };
 
 parseArgs();
