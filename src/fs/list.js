a309074d20d0262f6968dd44e2b96f95b5c19277
marteom <marteom@gmail.com>
2024-01-26 13:29:14 +0300

    feat: add solution for list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,31 @@
+import { readdir, access } from 'node:fs/promises';
+
+const checkExist = async (path) => {
+    try {
+        const checkResult = await access(path);
+        if(!checkResult) {
+            return true;
+        }
+    }
+    catch {
+        return false;
+    }
+}
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = new URL('files', import.meta.url);
+
+    const isSourceDirExist = await checkExist(dirPath);
+
+    if(!isSourceDirExist) {
+        throw new Error('FS operation failed');
+    }
+
+    const folderObjs = await readdir(dirPath, {withFileTypes: true});
+
+    for (const obj of folderObjs) {
+        console.log(obj.name);
+      }
 };
 
 await list();
