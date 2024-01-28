dfff2b6a7c38b5274194d19f40222b730c292cb0
marteom <marteom@gmail.com>
2024-01-28 18:22:35 +0300

    feat: add solution for child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+'use strict';
+
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import child_process from 'node:child_process';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const child = child_process.fork(__dirname + '/files/script.js', args);
+    
+    child.on('message', (code) =>
+        console.log(`Message to parent: ${code}`)
+    );
+    
+    child.on('close', (code) =>
+        console.log(`Child process exited. Code: ${code}`)
+    );
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(process.argv);
